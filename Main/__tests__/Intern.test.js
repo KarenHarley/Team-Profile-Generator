62282170582d569d840bdbb3ff502c1a183964d0
Karen V <karen.pretzel.365@gmail.com>
2021-09-03 21:00:12 -0500

    some changes

--- Main/__tests__/Intern.test.js
+++ Main/__tests__/Intern.test.js
@@ -1,8 +1,8 @@
 const Intern = require("../lib/Intern");
-
+//name, gender, age, project, id, email, school
 test("Can set school via constructor", () => {
   const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const e = new Intern("Sara","Female",21,"Tennis club Website",34,"Foo@gmail.com",testValue);
   expect(e.school).toBe(testValue);
 });
 
@@ -14,6 +14,6 @@ test("getRole() should return \"Intern\"", () => {
 
 test("Can get school via getSchool()", () => {
   const testValue = "UCLA";
-  const e = new Intern("Foo", 1, "test@test.com", testValue);
+  const e = new Intern("Sara","Female",21,"Tennis club Website",34,"Foo@gmail.com",testValue);
   expect(e.getSchool()).toBe(testValue);
 });
--- Main/__tests__/Manager.test.js
+++ Main/__tests__/Manager.test.js
@@ -3,18 +3,37 @@ const Employee = require("../lib/Employee");
 
 test("Can set office number via constructor argument", () => {
   const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  const e = new Manager("Foo", 1,"Female",19,"Tennis club Website",34,"Foo@gmail.com",testValue);
   expect(e.officeNumber).toBe(testValue);
 });
 
 test("getRole() should return \"Manager\"", () => {
   const testValue = "Manager";
-  const e = new Manager("Foo", 1, "test@test.com", 100);
+  const e = new Manager("Foo", 1,testValue,19,"Tennis club Website",34,"Foo@gmail.com",45);
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get office number via getOffice()", () => {
   const testValue = 100;
-  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  const e = new Manager("Foo", 1,"Female",19,"Tennis club Website",34,"Foo@gmail.com",testValue);
   expect(e.getOfficeNumber()).toBe(testValue);
+});
+/*Additional functionality */
+
+test("Can get gender via getGender()", () => {
+  const testValue = "Female";
+  const e = new Manager("Foo", 1,testValue,19,"Tennis club Website",34,"Foo@gmail.com",45);
+  expect(e.getGender()).toBe(testValue);
+});
+
+test("Can get age of manager via getAge()", () => {
+  const testValue = 19;
+  const e = new Manager("Foo", 1,"Female",testValue,"Tennis club Website",34,"Foo@gmail.com",45);
+  expect(e.getAge()).toBe(testValue);
+});
+
+test("Can get current project via getCurrentProject()", () => {
+  const testValue = "Tennis club Website";
+  const e = new Manager("Foo", 1,testValue,19,testValue,34,"Foo@gmail.com",45);
+  expect(e.getCurrentProject()).toBe(testValue);
 });
